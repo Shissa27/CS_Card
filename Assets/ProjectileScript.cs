8c244ae59615644d63b954b8d395099907b0d19e
Towich <towichf@gmail.com>
2023-04-03 16:26:32 +0300

    improved code visibility

--- Assets/ProjectileScript.cs
+++ Assets/ProjectileScript.cs
@@ -3,15 +3,16 @@ using UnityEngine;
 
 public class ProjectileScript : MonoBehaviour
 {
-    public float Speed = 10f;
-    public bool _toMove;
-    
+    private const float _speed = 10f;
+
     private GameObject _projectilePrefab;
     private GameObject _projectile;
 
     private Transform _startPosition;
     private Transform _destination;
     private Vector3 _direction;
+    
+    private bool _toMove;
     
     private void Start()
     {
@@ -20,14 +21,18 @@ public class ProjectileScript : MonoBehaviour
 
     private void Update()
     {
-        _projectile.transform.position += _direction * (Speed * Time.deltaTime);
+        // moving the projectile
+        _projectile.transform.position += _direction * (_speed * Time.deltaTime);
 
+        // check if projectile hit the enemy
         var curPos = _projectile.transform.position;
         if (Math.Abs(curPos.x - _destination.position.x) < 0.3f && 
             Math.Abs(curPos.y - _destination.position.y) < 0.3f)
         {
-            Destroy(_projectile);
-            Destroy(this);
+            Destroy(_projectile);   // destroy the projectile
+            Destroy(this);          // remove this component from Figure
+            
+            // call method "OnProjectiveHit"
             _destination.gameObject.GetComponent<TapCharacter>().OnProjectiveHit(_startPosition.gameObject);
         }
         
@@ -51,7 +56,5 @@ public class ProjectileScript : MonoBehaviour
         _direction = _destination.position - position;
         var angle = Mathf.Atan2(_direction.y, _direction.x) * Mathf.Rad2Deg;
         _projectile.transform.rotation = Quaternion.Euler(0, 0, angle);
-
-        
     }
 }
